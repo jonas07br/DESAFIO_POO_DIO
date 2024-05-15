8606aed6ce08e8ed6304a61b4abdcff152369482
jonas07br <jonasrafa07@gmail.com>
2024-05-15 14:20:39 -0300

    update contudo

--- src/Conteudo.java
+++ src/Conteudo.java
@@ -3,10 +3,12 @@ public abstract class Conteudo {
 
     private String titulo;
     private String descricao;
+    
     public Conteudo(){
         this.titulo = "TITULO";
         this.descricao = "DESCRIÇAO";
     }
+
     public Conteudo(String titulo,String descricao){
         this.titulo=titulo;
         this.descricao=descricao;
