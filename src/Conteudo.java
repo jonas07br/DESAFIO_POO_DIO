7d29b6efa9fd8f5c1743f6e099b67b755239b342
jonas07br <jonasrafa07@gmail.com>
2024-05-15 13:38:58 -0300

    add conteudo

---
+++ src/Conteudo.java
@@ -0,0 +1,29 @@
+public abstract class Conteudo {
+    protected static final double XP_PADRAO = 10;
+
+    private String titulo;
+    private String descricao;
+    public Conteudo(){
+        this.titulo = "TITULO";
+        this.descricao = "DESCRIÇAO";
+    }
+    public Conteudo(String titulo,String descricao){
+        this.titulo=titulo;
+        this.descricao=descricao;
+    }
+
+    public abstract double calcularXp();
+
+    public static double getXpPadrao() {
+        return XP_PADRAO;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+}
